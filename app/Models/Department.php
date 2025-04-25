fe096c7cf3fc74be6151d0d06a093f2e9916622c
Alexandre Martins <alemartins720@gmail.com>
2025-04-25 14:34:43 -0300

    Aula 263-Vamos testar os dados do admin

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    use HasFactory;
+
+    public function users()
+    {
+        // Each department can be filled with multiples users
+        return $this->belongsToMany(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
@@ -14,4 +15,9 @@ Route::get('/email', function () {
     });
 
     echo "Emai enviado com sucesso!";
+});
+
+Route::get('/admin' , function(){
+    $admin = User::with('detail' , 'department')->find(1);
+    dd($admin->toArray());
 });
