8fee51909453433e8b6f17abd6d0aa115e90084e
Alexandre Martins <alemartins720@gmail.com>
2025-04-25 12:24:46 -0300

    Aula 260-Vamos usar o MailPit localmente e testar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "RH MANGNT";
+
+Route::get('/email', function () {
+
+    Mail::raw('Mensagem de teste de envio de email', function (Message $message) {
+        $message->to('teste@gmail.com')
+            ->subject("Bem-Vindo ao RH MANGNT")
+            ->from('rh@rhmangnt.com');
+    });
+
+    echo "Emai enviado com sucesso!";
 });
