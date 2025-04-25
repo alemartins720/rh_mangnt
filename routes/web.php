510b0f304c9a07e279c970e5985eb9774a985982
Alexandre Martins <alemartins720@gmail.com>
2025-04-25 15:18:36 -0300

    Aula 265-Preparar layout da aplicação

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Mail\Message;
-use Illuminate\Support\Facades\Mail;
+
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/email', function () {
-
-    Mail::raw('Mensagem de teste de envio de email', function (Message $message) {
-        $message->to('teste@gmail.com')
-            ->subject("Bem-Vindo ao RH MANGNT")
-            ->from('rh@rhmangnt.com');
-    });
-
-    echo "Emai enviado com sucesso!";
-});
-
-Route::get('/admin' , function(){
-    $admin = User::with('detail' , 'department')->find(1);
-    return view('admin', compact('admin'));
-});
+Route::view('/login', 'auth.login');
