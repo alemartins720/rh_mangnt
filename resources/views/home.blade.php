fb12236504f4401aa572fb09780c86183193f9cc
Alexandre Martins <alemartins720@gmail.com>
2025-04-28 12:13:24 -0300

    Aula 268-Componente UserBar e Logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,7 @@
 <x-layout-app page-title="Home">
 
-    <h1 class="text-center">DENTRO DA APP</h1>
+    <h1 class="text-center my-5">DENTRO DA APP</h1>
 
-    @php
-        dump(auth()->user());
-    @endphp
+   
 
 </x-layout-app>
