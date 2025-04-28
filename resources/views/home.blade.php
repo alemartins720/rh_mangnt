4daeec231965a3babacc764964ff87680af501dd
Alexandre Martins <alemartins720@gmail.com>
2025-04-28 11:49:05 -0300

    Aula 267-Formulário de login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,8 @@
 
     <h1 class="text-center">DENTRO DA APP</h1>
 
+    @php
+        dump(auth()->user());
+    @endphp
+
 </x-layout-app>
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth')->group(function(){
+
+    Route::redirect('/' , 'home')->name('home');
     Route::view('/home' , 'home');
 });
