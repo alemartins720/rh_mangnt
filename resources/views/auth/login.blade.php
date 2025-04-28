8771b3cc6ec8ddad4153c1ca78a132cb9243a154
Alexandre Martins <alemartins720@gmail.com>
2025-04-28 13:53:10 -0300

    Aula 271-Reset de senha - Parte 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,6 +36,12 @@
                         </div>
     
                     </form>
+
+                    @if (session('status'))
+                        <div class="alert alert-success mt-3 text-center">
+                            {{ session('status') }}
+                        </div>
+                    @endif
     
                 </div>
     
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -34,8 +34,8 @@
                         </div>
                         
                         <div class="mb-3">
-                            <label for="password_confirmation ">Confirmar Senha</label>
-                            <input type="password" class="form-control" id="password_confirmation " name="password_confirmation ">
+                            <label for="password_confirmation">Confirmar Senha</label>
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
                             @error('password_confirmation')
                                 <div class="text-danger">{{ $message }}</div>
                             @enderror
