b8b93c50c8a9f2375f5cbf6bcf344a0ad55ea908
Alexandre Martins <alemartins720@gmail.com>
2025-04-28 14:24:15 -0300

    Aula 272-Gate Para o Perfil Admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 <x-layout-app page-title="Home">
 
-    <h1 class="text-center my-5">DENTRO DA APP</h1>
-
+   @can('admin')
+       <h3 class="text-center mt-5">É o usuário Admin que está logado!</h3>
+   @endcan
    
 
 </x-layout-app>
