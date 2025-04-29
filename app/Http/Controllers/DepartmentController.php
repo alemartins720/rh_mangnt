eff5e7a378cc60117e89102e58a154eede3c323d
Alexandre Martins <alemartins720@gmail.com>
2025-04-29 14:30:14 -0300

    Aula 280-Gestão de Departamentos parte 2

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class DepartmentController extends Controller
 {
     public function index()
     {
+        Auth::user()->can('admin') ?: abort(403 ,'You are not authorized to access this page!');
 
         $departments = Department::all();
 
--- resources/views/department/departments.blade.php
+++ resources/views/department/departments.blade.php
@@ -20,12 +20,15 @@
 
             <table class="table w-50" id="table">
                 <thead class="table-dark">
-                    <th>Department</th>
+                    <th>Departments</th>
                     <th></th>
                 </thead>
                 <tbody>
+
+                    @foreach ($departments as $department )
+                        
                     <tr>
-                        <td>[Department Name]</td>
+                        <td>{{ $department->name }}</td>
                         <td>
                             <div class="d-flex gap-3 justify-content-end">
                                 <a href="#" class="btn btn-sm btn-outline-dark"><i class="fa-regular fa-pen-to-square me-2"></i>Edit</a>
@@ -33,6 +36,8 @@
                             </div>
                         </td>
                     </tr>
+
+                    @endforeach                
                 </tbody>
             </table>
     </div>
