74ac3c302cb9741125b954b7ae7e0ed1b6108a69
Alexandre Martins <alemartins720@gmail.com>
2025-04-29 14:59:32 -0300

    Aula 281-Adicionar Departamento

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -5,17 +5,39 @@ namespace App\Http\Controllers;
 use App\Models\Department;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\View\View;
 
 class DepartmentController extends Controller
 {
     public function index()
     {
-        Auth::user()->can('admin') ?: abort(403 ,'You are not authorized to access this page!');
+        Auth::user()->can('admin') ?: abort(403, 'You are not authorized to access this page!');
 
         $departments = Department::all();
 
-        return view('department.departments' , compact('departments'));
+        return view('department.departments', compact('departments'));
+    }
 
+    public function newDepartment(): View
+    {
+        Auth::user()->can('admin') ?: abort(403, 'You are not authorized to access this page!');
+
+        return view('department.add-department');
+    }
+
+    public function createDepartment(Request $request)
+    {
+        Auth::user()->can('admin') ?: abort(403, 'You are not authorized to access this page!');
+
+        // Form validation
+        $request->validate([
+            'name' => 'required|string|max:50|unique:departments'
+        ]);
+
+        Department::create([
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('departments');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth')->group(function(){
+Route::middleware('auth')->group(function () {
 
-    Route::redirect('/' , 'home')->name('home');
-    Route::view('/home' , 'home');
+    Route::redirect('/', 'home')->name('home');
+    Route::view('/home', 'home');
 
     // Use profile page
-    Route::get('/user/profile' , [ProfileController::class , 'index' ])->name('user.profile');
-    Route::post('/user/profile/update-password' , [ProfileController::class , 'updatePassword'])->name('user.profile.update-password');
-    Route::post('/user/profile/update-user-data' , [ProfileController::class , 'updateUserData'])->name('user.profile.update-user-data');
+    Route::get('/user/profile', [ProfileController::class, 'index'])->name('user.profile');
+    Route::post('/user/profile/update-password', [ProfileController::class, 'updatePassword'])->name('user.profile.update-password');
+    Route::post('/user/profile/update-user-data', [ProfileController::class, 'updateUserData'])->name('user.profile.update-user-data');
 
     // Department route
-    Route::get('departments' , [DepartmentController::class , 'index'])->name('departments');
+    Route::get('/departments', [DepartmentController::class, 'index'])->name('departments');
+    Route::get('/departments/new-department', [DepartmentController::class, 'newDepartment'])->name('departments.new-department');
+    Route::post('/departments/create-department', [DepartmentController::class, 'createDepartment'])->name('departments.create-department');
 });
