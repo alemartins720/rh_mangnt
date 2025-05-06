df4fa315aa7ef892d5cfb6c13917886e8d6c6a6b
Alexandre Martins <alemartins720@gmail.com>
2025-05-06 16:28:32 -0300

    Aula 299-Vamos Enviar email para confirmar conta de colaborador parte 1

---
+++ app/Http/Controllers/ConfirmAccountController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConfirmAccountController extends Controller
+{
+    public function confirmAccount($url)
+    {
+        echo 'Estou aqui: ' . $url;
+    }
+}
---
+++ resources/views/mail/confirm-account-email.blade.php
@@ -0,0 +1,5 @@
+<p>Bem-vindo à confirmação de registro na aplicação {{ env('APP_NAME') }}!</p>
+<p>Para concluir o seu registro, clique no link abaixo para definir sua password</p>
+<p><a href="{{ $url }}">Clique aqui para definir a sua password</a></p>
+<p>Obrigado!</p>
+<p>A gestão de {{ env('APP_NAME') }}</p>
