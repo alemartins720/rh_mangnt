cb82954608cc2d57052e6eb1b9dfefc4e85eaa1f
Alexandre Martins <alemartins720@gmail.com>
2025-05-06 15:59:16 -0300

    Aula 297-Temos que fazer um refactoring das rotas

--- resources/views/colaborators/delete-rh-user.blade.php
+++ resources/views/colaborators/delete-rh-user.blade.php
@@ -11,7 +11,7 @@
         <div class="text-center">
             <h3 class="my-5">{{ $colaborator->name}}</h3>
             <a href="{{ route('colaborators.rh-users') }}" class="btn btn-secondary px-5">No</a>
-            <a href="{{ route('colaborators.delete-confirm' , ['id' => $colaborator->id]) }}" class="btn btn-danger px-5">Yes</a>
+            <a href="{{ route('colaborators.rh.delete-confirm' , ['id' => $colaborator->id]) }}" class="btn btn-danger px-5">Yes</a>
         </div>
         
     </div>
--- resources/views/colaborators/rh-users.blade.php
+++ resources/views/colaborators/rh-users.blade.php
@@ -10,11 +10,11 @@
         @if ($colaborators->count() === 0)
             <div class="text-center my-5">
                 <p>No colaborators found</p>
-                <a href="{{ route('colaborators.new-colaborator') }}" class="btn btn-primary">Create a new colaborator</a>
+                <a href="{{ route('colaborators.rh.new-colaborator') }}" class="btn btn-primary">Create a new colaborator</a>
             </div>
         @else
             <div class="mb-3">
-                <a href="{{ route('colaborators.new-colaborator') }}" class="btn btn-primary">Create a new colaborator</a>
+                <a href="{{ route('colaborators.rh.new-colaborator') }}" class="btn btn-primary">Create a new colaborator</a>
             </div>
         
 
@@ -43,8 +43,8 @@
                             <td>
 
                                 <div class="d-flex gap-3 justify-content-end">
-                                        <a href="{{ route('colaborators.edit-colaborator' , ['id' => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-regular fa-pen-to-square me-2"></i>Edit</a>
-                                        <a href="{{ route('colaborators.delete' , ['id' => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-regular fa-trash-can me-2"></i>Delete</a>
+                                        <a href="{{ route('colaborators.rh.edit-colaborator' , ['id' => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-regular fa-pen-to-square me-2"></i>Edit</a>
+                                        <a href="{{ route('colaborators.rh.delete-colaborator' , ['id' => $colaborator->id]) }}" class="btn btn-sm btn-outline-dark ms-3"><i class="fa-regular fa-trash-can me-2"></i>Delete</a>
                                      </div>
                             </td>
                         </tr>
