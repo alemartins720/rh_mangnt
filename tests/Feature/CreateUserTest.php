93d6572089287a524f204bc39a28a568a3417d79
Alexandre Martins <alemartins720@gmail.com>
2025-06-10 12:28:45 -0300

    Modificação no arquivo CreateUserTest

--- tests/Feature/CreateUserTest.php
+++ tests/Feature/CreateUserTest.php
@@ -34,25 +34,67 @@ it('Tests if an admin can insert a new Rh user', function () {
         'city' => '1234-City 1',
         'phone' => '123456789',
         'salary' => '1000.00',
-        'admission_date' => "2025-06-09",
+        'admission_date' => "2021-01-10",
         'role' => 'rh',
-        'permissions' => '["rh"]'
+        'permissions' => '["rh"]',
     ]);
 
     // Verifica se o user RH foi inserido com sucesso
     $this->assertDatabaseHas('users', [
         'name' => 'RH user 1',
-        'email' => 'rhuser@gmail.com',
+        'email' =>'rhuser@gmail.com',
         'role' => 'rh',
-        'permissions' => '["rh"]'
+        'permissions' => '["rh"]',
     ]);
 });
 
-function addDepartment($name)
-{
+// it('Tests if an RH users can insert a new colaborator', function () {
+
+//     // Criar user 
+//     addAdminUser();
+
+//     // Criar os departamentos
+//     addDepartment('Administração');
+//     addDepartment('Recursos Humanos');
+
+//     // Login com admin
+//     $result = $this->post('/login', [
+//         'email' => 'admin@rhmangnt.com',
+//         'password' => 'Aa123456'
+//     ]);
+
+//     // Verifica se o login foi feito com sucesso
+//     expect($result->status())->toBe(302);
+//     expect($result->assertRedirect('/home'));
+
+//     // Verifica se o admin consegue adicionar user do RH
+//     $result = $this->post('/rh-users/create-colaborator', [
+//         'name' => 'RH user',
+//         'email' => 'rh2@rhmangnt.com',
+//         'select_department' => 2,
+//         'address' => 'Rua dez',
+//         'zip_code' => '1234-123',
+//         'city' => 'Lisboa',
+//         'phone' => '123456789',
+//         'salary' => '1000.00',
+//         'admission_date' => "2025-06-09",
+//         'role' => 'rh',
+//         'permissions' => '["rh"]'
+//     ]);
+
+//     // Verifica se o user RH foi inserido com sucesso
+//     $this->assertDatabaseHas('users', [
+//         'name' => 'RH user',
+//         'email' => 'rh2@rhmangnt.com',
+//         'role' => 'rh',
+//         'permissions' => '["rh"]'
+//     ]);
+// });
+
+function addDepartment($name){
     Department::insert([
         'name' => $name,
         'created_at' => now(),
-        'updated_at' => now()
+        'updated_at' => now(),
     ]);
 }
