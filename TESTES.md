e077a93dab751a88d228324d38fc77c8d67c1a6d
Alexandre Martins <alemartins720@gmail.com>
2025-06-11 15:05:41 -0300

    Testar se não é possível acessar a home page sem estar logado

--- TESTES.md
+++ TESTES.md
@@ -12,7 +12,7 @@
 - [OK] Testar a inserção de um novo colaborador com perfil de RH
 
 - [OK] Testar se um admin, após login com sucesso, consegue ver a página de colaboradores rh
-- [] Testar se, sem usuário logado, é possível acessar a pagina Home
+- [OK] Testar se, sem usuário logado, é possível acessar a pagina Home
 - [] Testar se, com usuário logado, é possível acessar a pagina de Login
 - [] Testar se, estando logado, é possível acessar a pagina de recuperação de senha
 
--- tests/Feature/AccessTest.php
+++ tests/Feature/AccessTest.php
@@ -1,6 +1,8 @@
 <?php
 
-it('Tests is an admin user can see the RH users page' , function() {
+use Nette\Localization\ITranslator;
+
+it('Tests is an admin user can see the RH users page', function () {
 
     // Criar o admin
     addAdminUser();
@@ -10,5 +12,15 @@ it('Tests is an admin user can see the RH users page' , function() {
 
     // Verifica se acessa com sucesso a pagina de RH users
     expect($this->get('/rh-users')->status())->toBe(200);
+});
+
+it('Tests if is not possible to acces the home page without logged user' , function(){
+
+    // Verifica se é possível acessar a home page
+    expect($this->get('/home')->status())->toBe(302);
+
+    // ou
+
+    expect($this->get('/home')->status())->not()->toBe(200);
 
 });
